1382fd17010ab4391e6b7b5495ee61cbbdcaf935
Drazen Strok <stroky25@gmail.com>
2026-05-04 18:32:24 +0200

    Treći Commit - Bez Testne linije

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main (String[] args) {
             LocalDateTime dt = LocalDateTime.now();
             System.out.println(dt);
-            System.out.println("Test");
     }
 }
