3b883790184921d8629a6a2e40c9d04aa5851d03
Drazen Strok <stroky25@gmail.com>
2026-05-04 18:29:54 +0200

    Drugi Commit - Test

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main (String[] args) {
             LocalDateTime dt = LocalDateTime.now();
             System.out.println(dt);
+            System.out.println("Test");
     }
 }
