64893aa853ba58e4269c84fd9766f8b483dacee4
stroky25 <84547159+stroky25@users.noreply.github.com>
2026-05-04 18:57:28 +0200

    dodane sekunde i ukupno vrijeme

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main (String[] args) {
             LocalDateTime dt = LocalDateTime.now();
             System.out.println(dt);
+            System.out.println ("Dan: "+ dt.getDayOfMonth());
+            System.out.println ("Mjesec: "+ dt.getMonth());
+            System.out.println ("Sat: "+ dt.getHour());
+            System.out.println ("Sekunde: "+ dt.getSecond());
+            System.out.println ("Vrijeme ukupno: "+ dt());               
     }
 }
